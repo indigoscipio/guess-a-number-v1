c6674ae7d57efe201e9147dbf7549e084dd4c331
unknown <samueloctavianus97@gmail.com>
2022-05-02 11:23:17 +0700

    finalization

--- guess-a-number.js
+++ guess-a-number.js
@@ -3,6 +3,7 @@ let btnCheck = document.querySelector("#check")
 let notificationPopUp = document.querySelector(".notification");
 let notificationText = document.querySelector("#notification-text")
 let btnTryAgain = document.querySelector(".try-again")
+let textScore = document.querySelector("#text-score");
 
 //check if a number is greater or less than 20
     //if its greater, pop up the notification
@@ -11,7 +12,8 @@ let btnTryAgain = document.querySelector(".try-again")
 
 var randomNum = Math.floor(Math.random()*20 +1 )
 btnCheck.addEventListener("click", checkNum);
-var score = 0;
+btnTryAgain.addEventListener("click", reset);
+var score = 20;
 var highScore = 0; 
 console.log(notificationPopUp);
 console.log(btnTryAgain);
@@ -33,17 +35,44 @@ function checkNum(){
         notificationPopUp.classList.remove("hidden");
         btnTryAgain.classList.remove("hidden");
         notificationPopUp.style.backgroundColor = "green";
-        notificationText.innerText = "You are correct!!!!";
+        notificationText.innerText = "You are correct!! Game Over. 🎉🙌";
         console.log("Correct!!!!");
+        gameEnd();
     }
 
+    showScore();
     console.log(`Your answer: ${userGuessNum}`);
     console.log(`the random number is: ${randomNum}`)
 }
 
+//A function that ends the game
+function gameEnd(){
+    btnCheck.removeEventListener("click", checkNum);
+    btnCheck.classList.add("disabled");
+    numInput.disabled = true;
+}
+
+//a function that resets the game
+
+function reset(){
+    location.reload();
+}
+
 //A function keeps track of score & high score
-function scoreCheck(){
-    //runs checkNum
+function showScore(){
+    //runs checkNum, if guess is correct, increment by 1
+    if(notificationPopUp.style.backgroundColor !== "green"){
+        console.log(score);
+        if(score > 0){
+            score--;
+            textScore.innerText = score;
+        }
+        else{ 
+            score = 0;
+            textScore.innerText = score;
+        }
+    }
+
     //if it's score, keeps the return value of that game.
     //if it's high score, create a loop once the user hits the "Try again button".
 }
